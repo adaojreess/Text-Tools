80231fb74a18b4fef28f113b28c5720f0e54de44
adaojreess <adaojreess@gmail.com>
2021-03-16 11:32:13 -0300

    create contant page

---
+++ lib/views/content/content_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ContentPage extends StatefulWidget {
+  @override
+  _ContentPageState createState() => _ContentPageState();
+}
+
+class _ContentPageState extends State<ContentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
